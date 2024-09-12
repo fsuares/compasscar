114726b891cff8a7b103baac68936ece968e7256
Fernando Suares <fernandoaqs@outlook.com.br>
2024-09-12 14:52:09 -0300

    style: fix query names

--- models/carsModel.js
+++ models/carsModel.js
@@ -7,22 +7,22 @@ const getCars = async (params) => {
 
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
-    const querry = `SELECT * FROM cars`
+    const query = `SELECT * FROM cars`
 
     if (brand || model || year) {
-        querry += ` WHERE `
+        query += ` WHERE `
         if (brand) {
-            querry += `brand = '${brand}'`
+            query += `brand = '${brand}'`
         }
         if (model) {
-            querry += `model = '${model}'`
+            query += `model = '${model}'`
         }
         if (year) {
-            querry += `year = '${year}'`
+            query += `year = '${year}'`
         };
     };
 
-    const [cars] = await connection.execute(querry);
+    const [cars] = await connection.execute(query);
     const pages = Math.ceil(cars.length / limit);
     const cars_limited = cars.slice(startIndex, endIndex);
     return { "count": cars.length, "pages": pages, "data": cars_limited };
@@ -30,7 +30,7 @@ const getCars = async (params) => {
 
 const getCarsByID = async (params) => {
     const { id } = params;
-    const querry = `SELECT
+    const query = `SELECT
                         cars.id,
                         cars.brand,
                         cars.model,
@@ -45,20 +45,20 @@ const getCarsByID = async (params) => {
                         cars.id, cars.brand, cars.model, cars.year
                     ORDER BY
                         cars.id;`
-    const [car] = await connection.execute(querry, [id]);
+    const [car] = await connection.execute(query, [id]);
     return car;
 };
 
 const addCar = async (car) => {
     const { brand, model, year } = car;
     const items = [...new Set(car.items)];
-    const querry_car = `INSERT INTO cars (brand, model, year)
+    const query_car = `INSERT INTO cars (brand, model, year)
                     VALUES ( ? , ? , ? );`
-    const querry_items = `INSERT INTO cars_items (name, car_id)
+    const query_items = `INSERT INTO cars_items (name, car_id)
                     VALUES ( ? , ? );`
-    const createdCar = await connection.execute(querry_car, [brand, model, year]);
+    const createdCar = await connection.execute(query_car, [brand, model, year]);
     items.forEach(item => {
-        const createdItems = connection.execute(querry_items, [item, createdCar[0].insertId]);
+        const createdItems = connection.execute(query_items, [item, createdCar[0].insertId]);
     });
 
     return createdCar[0].insertId;
@@ -80,26 +80,26 @@ const updateCar = async (id, car) => {
     params.push(id);
 
     if (brand || model || year) {
-        let querry_car = `UPDATE cars
+        let query_car = `UPDATE cars
                             SET
                             ${brand ? 'brand = ?, ' : ''}
                             ${model ? 'model = ?, ' : ''}
                             ${year ? 'year = ?, ' : ''}`;
 
-        querry_car = querry_car.trim().slice(0, -1);
-        querry_car += ` WHERE id = ?;`
+        query_car = query_car.trim().slice(0, -1);
+        query_car += ` WHERE id = ?;`
 
-        const updatedCar = await connection.execute(querry_car, params);
+        const updatedCar = await connection.execute(query_car, params);
     };
 
     if (items) {
         const dedup_items = [...new Set(items)];
-        const querry_delete_items = `DELETE FROM cars_items WHERE car_id = ?;`
-        const querry_items = `INSERT INTO cars_items (name, car_id)
+        const query_delete_items = `DELETE FROM cars_items WHERE car_id = ?;`
+        const query_items = `INSERT INTO cars_items (name, car_id)
                         VALUES ( ? , ? );`;
-        const removeItems = await connection.execute(querry_delete_items, [id]);
+        const removeItems = await connection.execute(query_delete_items, [id]);
         dedup_items.forEach(item => {
-            const createdItems = connection.execute(querry_items, [item, id]);
+            const createdItems = connection.execute(query_items, [item, id]);
         });
     };
 
